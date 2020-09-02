e4e875074f6483223424601f39e31ea591aa65d7
Andarra28 <Andarra28@gmail.com>
2020-09-02 09:34:13 +0700

    laravel-andarra

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Andarra', function() {
+    return 'Ohayo Minasan';
+});
+
+Route::get ('/Belajar', function() {
+    echo '<h1> Hello EveryBody </h1>';
+});
